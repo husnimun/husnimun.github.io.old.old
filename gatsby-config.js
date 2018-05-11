da4a012971e7992f8d8935256a0772d1078d4313
Husni Munaya <husnimunaya@gmail.com>
2018-05-11 10:52:19 +0700

    Add gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Husni Munaya',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
