5d757532b5529378a0a78972a56f1a49d352e877
Husni Munaya <husnimunaya@gmail.com>
2018-05-25 02:20:11 +0700

    Refactor

--- src/components/styled/Link.js
+++ src/components/styled/Link.js
@@ -1,53 +1,48 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Link from 'gatsby-link'
 
-export const StyledAnchor = styled(({ light, ...rest }) => <a {...rest} />)`
+const baseStyles = css`
   position: relative;
-  color: ${props => (props.light ? '#2b2b2b' : '#221C2F')};
   text-decoration: none;
   transition: 300ms ease;
   z-index: 100;
-
-  &:before {
-    content: '';
-    position: absolute;
-    width: calc(100%);
-    height: 3px;
-    border-radius: 2px;
-    background: ${props =>
-      props.light ? 'rgba(255, 255, 255, 0.8)' : 'rgba(72, 57, 120, 0.6);'};
-    bottom: 0px;
-    z-index: -1;
-    left: 0;
-  }
 
   &:hover {
     color: #5f5f5f;
   }
 `
 
+const fontColor = ({ light }) => {
+  const color = light ? '#2b2b2b' : '#221C2F'
+  return css`
+    color: ${color};
+  `
+}
+
+const underlineStyles = ({ light }) => {
+  const backgroundColor = light
+    ? 'rgba(255, 255, 255, 0.8)'
+    : 'rgba(72, 57, 120, 0.6)'
+  return css`
+    &:before {
+      content: '';
+      position: absolute;
+      width: calc(100%);
+      height: 3px;
+      border-radius: 2px;
+      background: ${backgroundColor};
+      bottom: 0px;
+      z-index: -1;
+      left: 0;
+    }
+  `
+}
+
+export const StyledAnchor = styled(({ light, ...rest }) => <a {...rest} />)`
+  ${baseStyles} ${fontColor} ${underlineStyles};
+`
+
 export const StyledLink = styled(({ light, ...rest }) => <Link {...rest} />)`
-  position: relative;
-  color: ${props => (props.light ? '#2b2b2b' : '#221C2F')};
-  text-decoration: none;
-  transition: 300ms ease;
-  z-index: 100;
-
-  &:before {
-    content: '';
-    position: absolute;
-    width: calc(100%);
-    height: 3px;
-    border-radius: 2px;
-    background: ${props =>
-      props.light ? 'rgba(255, 255, 255, 0.8)' : 'rgba(72, 57, 120, 0.6);'};
-    bottom: 0px;
-    z-index: -1;
-    left: 0;
-  }
-
-  &:hover {
-    color: #5f5f5f;
-  }
+  ${baseStyles} ${fontColor} ${underlineStyles};
 `
