d2a8a396689d11aec6a66bc16d15a57d03051d67
Husni Munaya <husnimunaya@gmail.com>
2018-06-08 11:00:56 +0700

    Add nprogress

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Husni Munaya',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#36c9e8`,
+        showSpinner: false,
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+  ],
 }
